dc91e2b5109d1b44742edab79f5a04ea398f628b
Christopher <cmullins@liveramp.com>
2015-07-10 08:17:53 -0700

    Move methods

--- lib/bravtroller/client.rb
+++ lib/bravtroller/client.rb
@@ -3,25 +3,8 @@ require 'json'
 
 module Bravtroller
   class Client
-    LIST_METHODS_PARAMS = {
-        method: 'getRemoteControllerInfo',
-        params: [],
-        id: 10,
-        version: '1.0'
-    }
-
     def initialize(bravia_address)
       @bravia_address = bravia_address
-    end
-
-    def ircc_codes
-      response = JSON.parse(post_request('/sony/system', LIST_METHODS_PARAMS).body)
-
-      action_code_pairs = response['result'][1].map do |action|
-        [ action['name'], action['value'] ]
-      end
-
-      Hash[ action_code_pairs ]
     end
 
     def post_request(path, params = {}, headers = {})
--- lib/bravtroller/remote.rb
+++ lib/bravtroller/remote.rb
@@ -1,9 +1,36 @@
-require 'easy_upnp'
+require 'easy_upnp/ssdp_searcher'
+
+require_relative 'authenticator'
 
 module Bravtroller
   class Remote
     def initialize(ircc_client)
       @ircc_client = ircc_client
+      @bravia_client = bravia_client
+    end
+
+    def press_button(button_key)
+      raise RuntimeError.new "Undefined buton: #{button_key}" if ircc_codes[button_key].nil?
+
+      @ircc_client.X_SendIRCC IRCCCode: ircc_codes[button_key]
+    end
+
+    def buttons
+      ircc_codes.keys
+    end
+
+    private
+
+    def ircc_codes
+      return @ircc_codes unless @ircc_codes.nil?
+
+      response = JSON.parse(@bravia_client.post_request('/sony/system', LIST_METHODS_PARAMS).body)
+
+      action_code_pairs = response['result'][1].map do |action|
+        [ action['name'], action['value'] ]
+      end
+
+      Hash[ action_code_pairs ]
     end
   end
 end
